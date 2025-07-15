c6f86252ff56dd5dc1d2fd9143e02ca88259a09d
wjh <wjh2012@naver.com>
2025-07-15 17:30:41 +0900

    feat: 대용량손글씨

---
+++ one_by_one/large_handwriting/data_structure.py
@@ -0,0 +1,21 @@
+from typing import List
+
+import msgspec
+
+
+class Image(msgspec.Struct):
+    identifier: str
+    width: int
+    height: int
+
+
+class Bbox(msgspec.Struct):
+    data: str
+    id: int
+    x: List[int]
+    y: List[int]
+
+
+class LargeHandwritingData(msgspec.Struct):
+    images: Image = msgspec.field(name="Images")
+    bbox: List[Bbox] = msgspec.field(name="bbox")
